54999ca5103f18eab522f9cf4870ce5b0e4d74ba
Prem <premmittal88@gmail.com>
2023-10-01 19:19:25 +0530

    Filters added

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -30,7 +30,7 @@ const Products = () => {
         </div>
         <div className="col-md-9 ">
           <h1 className="text-center">All Products List</h1>
-          <div className="d-flex">
+          <div className="d-flex flex-wrap">
             {products?.map((p) => (
               <Link
                 key={p._id}
@@ -38,7 +38,7 @@ const Products = () => {
                 className="product-link"
               >
                 <div className="card m-2" style={{ width: "18rem"}}>
-                  <img style={{ height: "16rem", width: "16rem"}}
+                  <img 
                     src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
